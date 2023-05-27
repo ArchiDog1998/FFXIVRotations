d0af227098eed734dcda0ceb62c8824aadbb2354
秋水 <1123993881@qq.com>
2023-05-27 20:57:09 +0800

    fix: changed ko-fi link.

--- DefaultRotations/AssemblyInfo.cs
+++ DefaultRotations/AssemblyInfo.cs
@@ -1,4 +1,4 @@
 ﻿[assembly: AuthorHash("Ig4lHXUohMZNIeheUtAtRg==")]
 [assembly: AssemblyLink("https://discord.gg/4fECHunam9", "https://github.com/ArchiDog1998/FFXIVRotations",
     "https://github.com/ArchiDog1998/FFXIVRotations/blob/release/CHANGELOG.md",
-    "https://ko-fi.com/rotationsolver")]
+    "https://ko-fi.com/archited")]
