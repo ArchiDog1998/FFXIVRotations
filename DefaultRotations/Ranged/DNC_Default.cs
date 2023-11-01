6aee1cd10b966f054a8b7e160d0f238f7d22e395
秋水 <1123993881@qq.com>
2023-11-01 22:23:24 +0800

    fix: add tinctures for dnc. but it is not ideal.

--- DefaultRotations/Ranged/DNC_Default.cs
+++ DefaultRotations/Ranged/DNC_Default.cs
@@ -17,13 +17,30 @@ public sealed class DNC_Default : DNC_Base
         return base.CountDownAction(remainTime);
     }
 
+    protected override bool EmergencyAbility(IAction nextGCD, out IAction act)
+    {
+        if (IsDancing)
+        {
+            return base.EmergencyAbility(nextGCD, out act);
+        }
+
+        foreach (var action in RecordActions)
+        {
+            if (action.Action.RowId != (uint)ActionID.TechnicalStep) continue;
+
+            var timeFromNow = (DateTime.Now - action.UsedTime).TotalSeconds;
+            if (timeFromNow > 15) continue;
+
+            return UseBurstMedicine(out act);
+        }
+        return base.EmergencyAbility(nextGCD, out act);
+    }
+
     protected override bool AttackAbility(out IAction act)
     {
         act = null;
-        //跳舞状态禁止使用
         if (IsDancing) return false;
 
-        //进攻之探戈
         if (Devilment.CanUse(out act))
         {
             if (IsBurst && !TechnicalStep.EnoughLevel) return true;
@@ -31,24 +48,17 @@ public sealed class DNC_Default : DNC_Base
             if (Player.HasStatus(true, StatusID.TechnicalFinish)) return true;
         }
 
-        //应急换舞伴
         if (UseClosedPosition(out act)) return true;
 
-        //百花
         if (Flourish.CanUse(out act)) return true;
-
-        //扇舞·急
         if (FanDance3.CanUse(out act, CanUseOption.MustUse)) return true;
 
         if (Player.HasStatus(true, StatusID.Devilment) || Feathers > 3 || !TechnicalStep.EnoughLevel)
         {
-            //扇舞·破
             if (FanDance2.CanUse(out act)) return true;
-            //扇舞·序
             if (FanDance.CanUse(out act)) return true;
         }
 
-        //扇舞·终
         if (FanDance4.CanUse(out act, CanUseOption.MustUse))
         {
             if (TechnicalStep.EnoughLevel && TechnicalStep.IsCoolingDown && TechnicalStep.WillHaveOneChargeGCD()) return false;
@@ -60,94 +70,62 @@ public sealed class DNC_Default : DNC_Base
 
     protected override bool GeneralGCD(out IAction act)
     {
-        //绑定舞伴
         if (!InCombat && !Player.HasStatus(true, StatusID.ClosedPosition1) && ClosedPosition.CanUse(out act)) return true;
 
-        //结束舞步
         if (FinishStepGCD(out act)) return true;
-
-        //执行舞步
         if (ExecuteStepGCD(out act)) return true;
 
-        //技巧舞步
         if (IsBurst && InCombat && TechnicalStep.CanUse(out act, CanUseOption.MustUse)) return true;
 
-        //攻击GCD
         if (AttackGCD(out act, Player.HasStatus(true, StatusID.Devilment))) return true;
 
         return base.GeneralGCD(out act);
     }
 
-    /// <summary>
-    /// 攻击GCD
-    /// </summary>
-    /// <param name="act"></param>
-    /// <param name="breaking"></param>
-    /// <returns></returns>
-    private bool AttackGCD(out IAction act, bool breaking)
+    private static bool AttackGCD(out IAction act, bool breaking)
     {
         act = null;
-        //跳舞状态禁止使用
         if (IsDancing) return false;
 
-        //剑舞
         if ((breaking || Esprit >= 85) && SaberDance.CanUse(out act, CanUseOption.MustUse)) return true;
 
-        //流星舞
         if (StarFallDance.CanUse(out act, CanUseOption.MustUse)) return true;
 
-        //提拉纳
         if (Tillana.CanUse(out act, CanUseOption.MustUse)) return true;
 
-        //使用标准舞步
         if (UseStandardStep(out act)) return true;
 
-        //触发AOE
         if (BloodShower.CanUse(out act)) return true;
         if (FountainFall.CanUse(out act)) return true;
-        //触发单体
+
         if (RisingWindmill.CanUse(out act)) return true;
         if (ReverseCascade.CanUse(out act)) return true;
 
-        //基础AOE
         if (BladeShower.CanUse(out act)) return true;
         if (Windmill.CanUse(out act)) return true;
-        //基础单体
+
         if (Fountain.CanUse(out act)) return true;
         if (Cascade.CanUse(out act)) return true;
 
         return false;
     }
 
-    /// <summary>
-    /// 使用标准舞步
-    /// </summary>
-    /// <param name="act"></param>
-    /// <returns></returns>
-    private bool UseStandardStep(out IAction act)
+    private static bool UseStandardStep(out IAction act)
     {
         if (!StandardStep.CanUse(out act, CanUseOption.MustUse)) return false;
         if (Player.WillStatusEndGCD(2, 0, true, StatusID.StandardFinish)) return true;
 
-        //周围没有敌人不跳舞
         if (!HasHostilesInRange) return false;
 
-        //技巧舞步状态和快冷却好时不释放
         if (TechnicalStep.EnoughLevel && (Player.HasStatus(true, StatusID.TechnicalFinish) || TechnicalStep.IsCoolingDown && TechnicalStep.WillHaveOneCharge(5))) return false;
 
         return true;
     }
 
-    /// <summary>
-    /// 应急换舞伴
-    /// </summary>
-    /// <param name="act"></param>
-    /// <returns></returns>
-    private bool UseClosedPosition(out IAction act)
+    private static bool UseClosedPosition(out IAction act)
     {
         if (!ClosedPosition.CanUse(out act)) return false;
 
-        //应急换舞伴
         if (InCombat && Player.HasStatus(true, StatusID.ClosedPosition1))
         {
             foreach (var friend in PartyMembers)
@@ -159,9 +137,6 @@ public sealed class DNC_Default : DNC_Base
                 }
             }
         }
-        //else if (ClosedPosition.ShouldUse(out act)) return true;
-
-        act = null;
         return false;
     }
 
@@ -170,7 +145,6 @@ public sealed class DNC_Default : DNC_Base
         act = null;
         if (!IsDancing) return false;
 
-        //标准舞步结束
         if (Player.HasStatus(true, StatusID.StandardStep) && (Player.WillStatusEnd(1, true, StatusID.StandardStep) || CompletedSteps == 2 && Player.WillStatusEnd(1, true, StatusID.StandardFinish))
             || StandardFinish.CanUse(out _, CanUseOption.MustUse))
         {
