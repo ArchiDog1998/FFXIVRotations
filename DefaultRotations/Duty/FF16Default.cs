a6402ad2d911f2c6759f159974f2398be86a6fe2
秋水 <53346444+ArchiDog1998@users.noreply.github.com>
2024-04-09 23:41:50 +0800

    fix: add ff16 rotation.

---
+++ DefaultRotations/Duty/FF16Default.cs
@@ -0,0 +1,26 @@
+﻿using RotationSolver.Basic.Rotations.Duties;
+
+namespace DefaultRotations.Duty;
+
+[Rotation("FF16 Default", CombatType.PvE)]
+
+public class FF16Default : FF16Rotation
+{
+    public override bool EmergencyAbility(IAction nextGCD, out IAction? act)
+    {
+        if (VfxNewData.Any(d => d.Path == "vfx/common/eff/kaihi_stlp_c1v.avfx" && d.TimeDuration.TotalSeconds is > 1.2 and < 3))
+        {
+            if (DodgePvE_33997.CanUse(out act, skipClippingCheck: true)) return true;
+        }
+
+        return base.EmergencyAbility(nextGCD, out act);
+    }
+
+    public override bool AttackAbility(out IAction? act)
+    {
+        if (PrecisionStrikePvE.CanUse(out act, skipAoeCheck: true)) return true;
+        if (RisingFlamesPvE.CanUse(out act, skipAoeCheck: true)) return true;
+
+        return base.AttackAbility(out act);
+    }
+}
